3ccf081683a2961b90bafcfea206021c146b6bf6
Justin McNally <justin@kohactive.com>
2014-07-23 16:43:47 -0500

    update buildpak for api key

--- files/run_znc.sh
+++ files/run_znc.sh
@@ -1,6 +1,5 @@
-./znc/bin/znc
-./ngrok -authtoken $NGROK_API_KEY --config ngrok.conf start znc
-
+./znc/bin/znc -f &
+./ngrok -authtoken $NGROK_API_KEY --config ngrok.conf start znc &
 ps aux
 
 echo "Exiting?"
